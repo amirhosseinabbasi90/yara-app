487d3804d0be83079cfe9a620030290050f30c65
amirhosseinabbasi90 <amir.hossein.abbasi2025.90.91@gmail.com>
2026-07-31 13:47:34 +0330

    Create YaraColors class for design constants
    Added a new class YaraColors with design constants.

---
+++ lib/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+/// رنگ‌ها و مقادیر ثابت طراحی یارا، برای استفاده در تمام صفحات
+class YaraColors {
+  static const Color purple = Color(0xFF482E83);
+  static const Color darkPurple = Color(0xFF1E1338);
+  static const Color gold = Color(0xFFD4AF37);
+  static const Color bgSoft = Color(0xFFF7F5FB);
+  static const Color textDark = Color(0xFF231C33);
+}
+
